7a7097b93b136875c6bc81017109f4d72da11d99
byeongjuPark <bottlepark28@gmail.com>
2022-10-24 15:08:52 +0900

    123

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # 집에가고싶조 세미 프로젝트
-
-### 화면정의서
+### 프로젝트 구성
+#### 화면정의서
 - [화면정의서 링크](./src/%ED%99%94%EB%A9%B4%EC%A0%95%EC%9D%98%EC%84%9C(%EC%A7%91%EC%97%90%EA%B0%80%EA%B3%A0%EC%8B%B6%EC%A1%B0).pdf)
-### 요구사항정의서
-- [요구사항 정의서](./src/%EC%9A%94%EA%B5%AC%EC%82%AC%ED%95%AD%EC%A0%95%EC%9D%98%EC%84%9C(%EC%A7%91%EC%97%90%EA%B0%80%EA%B3%A0%EC%8B%B6%EC%A1%B0).xlsx.pdf)
-### ERD
-- [ERD png 파일](./src/Concert_Survey.png)
+#### 요구사항정의서
+- [요구사항 정의서](./src/%ED%99%94%EB%A9%B4%EC%A0%95%EC%9D%98%EC%84%9C(%EC%A7%91%EC%97%90%EA%B0%80%EA%B3%A0%EC%8B%B6%EC%A1%B0).pdf)
+#### ERD
+- [ERD png 파일](./src/Survey.png)
 ### SRC
 #### Class
 - [메인 클래스](./src/Main.java)
-- [음식 선호도 조사](./src/FoodSurvey.java)
 - [Ott 만족도 조사](./src/OttSurvey.java)
-- [콘서트 만족도 조사](./src/ConcertSurvey.java)
+- [통계](./src/Result.java)
+#### SQL
+- [Create Table](./src/Survey.sql)  
+- [INSERT](./src/insert.sql)  
+- [SELECT](./src/surveySQL.sql)  
+
 
 
---
+++ src/surveySQL.sql
@@ -0,0 +1,62 @@
+INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('1', '현재 구독중인 OTT 에서 제공하는 컨텐츠는 만족하는가');
+INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('2', 'OTT에서 추천해주는 컨텐츠는 만족하는가');
+INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('3', '업로드 주기는 적당하다고 생각하는가');
+INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('4', '현재 OTT에서 제공하고 있는 자막 제공 서비스에 만족 하는가');
+INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('5', '이용중인 구독료는 적당하다고 생각하는가');
+INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('6', '향후 구독을 계속 유지할 생각이 있는가');
+INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('7', '지인에게 해당 서비스를 추천할 의향이 있는가');
+
+
+INSERT INTO `semi_project`.`answers` (`ANSWER_UID`, `ANSWER`) VALUES ('1', '전혀 아니다');
+INSERT INTO `semi_project`.`answers` (`ANSWER_UID`, `ANSWER`) VALUES ('2', '아니다');
+INSERT INTO `semi_project`.`answers` (`ANSWER_UID`, `ANSWER`) VALUES ('3', '보통이다');
+INSERT INTO `semi_project`.`answers` (`ANSWER_UID`, `ANSWER`) VALUES ('4', '그렇다');
+INSERT INTO `semi_project`.`answers` (`ANSWER_UID`, `ANSWER`) VALUES ('5', '매우 그렇다');
+
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('1', '1');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('2', '1');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('3', '1');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('4', '1');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('5', '1');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('1', '2');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('2', '2');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('3', '2');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('4', '2');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('5', '2');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('1', '3');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('2', '3');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('3', '3');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('4', '3');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('5', '3');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('1', '4');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('2', '4');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('3', '4');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('4', '4');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('5', '4');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('1', '5');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('2', '5');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('3', '5');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('4', '5');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('5', '5');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('1', '6');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('2', '6');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('3', '6');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('4', '6');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('5', '6');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('1', '7');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('2', '7');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('3', '7');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('4', '7');
+INSERT INTO `semi_project`.`questions_answers` (`ANSWER_UID`, `QUESTIONS_UID`) VALUES ('5', '7');
+
+
+-- 회원 가입
+INSERT INTO `semi_project`.`participants` (`PARTICIPANTS_UID`, `NAME`) VALUES ('1', '박병주');
+-- 답안 출력
+SELECT answer FROM (questions
+INNER JOIN questions_answers ON questions_answers.QUESTIONS_UID = questions.QUESTIONS_UID)
+INNER JOIN answers ON questions_answers.ANSWER_UID = answers.ANSWER_UID
+WHERE questions.QUESTIONS_UID = '1';
+-- 사용자 답 입력
+INSERT INTO result (PARTICIPANTS_UID, ANSWER_UID, QUESTIONS_UID) VALUES ('1', '1', '1');
+
