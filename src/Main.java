b05f5114d98699f46bc3ba9280fdb3994fa74a6b
ParkChanhee1130 <kkazza1130@gmail.com>
2022-10-24 22:15:22 +0900

    main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.sql.*;
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-
+        
         String url = "jdbc:mysql://127.0.0.1:3306/study_sqls";
         String user = "root";
         String password = "root";
-
+        
+        Scanner sc = new Scanner(System.in);
         
 
         try{
@@ -22,12 +23,32 @@ public class Main {
             Result resultSurvey = new Result(statement);
             
     
+            while(true) {
+                System.out.println("----------------------------------------");
+                System.out.println("설문조사 프로그램 \n P. 설문 시작 \n S. 통계 \n Q. 종료");
+                System.out.println("----------------------------------------");
+                
+                System.out.println("선택 >> ");
+                String ch = sc.next();
+                
+                if(ch.equals("P")) {
+                    OttSurvey ott = new OttSurvey();
+                    ott.
+                } else if (ch.equals("S")) {
+                    Result result = new Result();
+                    result.
+                } else if (ch.equals("Q")) {
+                    System.out.println("답변해 주셔서 감사합니다.");
+                    break;
+                }
+            }
         }catch(SQLException e){
-
+            e.printStackTrace();
         }
 
-        while(true) {
-        }
+        sc.close();
 
+
+        
     }
 }
