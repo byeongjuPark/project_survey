aeeebf2381dff57bd299e3a0655dfbe4019ee660
ParkChanhee1130 <kkazza1130@gmail.com>
2022-10-25 15:55:14 +0900

    CHECK

--- src/Main.java
+++ src/Main.java
@@ -39,6 +39,7 @@ public class Main {
                     //ott.OttSurvey(statement);
                 } else if (ch.equals("S")) {
                     new Result().printCalMenu(connection ,statement, preparedStatement);
+                    new Result().calculateAnswers(connection ,statement, preparedStatement);
                 } else if (ch.equals("Q")) {
                     System.out.println("답변해 주셔서 감사합니다");
                     break;
