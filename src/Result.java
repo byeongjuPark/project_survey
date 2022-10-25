4ba4a5dae53ee0bf07abef5836f99d416ea1908b
byeongjuPark <bottlepark28@gmail.com>
2022-10-25 11:45:40 +0900

    정리

--- src/Result.java
+++ src/Result.java
@@ -30,22 +30,21 @@ public class Result {
     
     // 1. 설문자별 답변 결과
     public void calculateParticipants(Connection connection ,Statement statement, PreparedStatement preparedStatement){
-       
         try{
-           
-
             String query = "SELECT PARTICIPANTS_UID, NAME FROM participants";
+            //sql문 중간에 붙은 ? 는 밑에서 preparedStatement.setString()을 통해서 치환해줄 예정 
             String queryAnswer = "SELECT ANSWER_UID FROM RESULT WHERE PARTICIPANTS_UID = ? ORDER BY QUESTIONS_UID;";
             ResultSet resultSet = statement.executeQuery(query);
+            //PreparedStatement --> Statement 의 상위호환..? 동적으로 sql문을 실행하도록 도와줌
             preparedStatement = connection.prepareStatement(queryAnswer);
-            
-
             System.out.println("         Q1   Q2   Q3   Q4   Q5   Q6   Q7");
             System.out.println("이름)");
             while(resultSet.next()){
                 System.out.print(resultSet.getString("NAME")+"   ");
                 String PID = resultSet.getString("PARTICIPANTS_UID");
+                // setString(1, PID) -> 첫번째 ? 를 PID로 대체
                 preparedStatement.setString(1, PID);
+                //치환된 sql문 실행!
                 ResultSet resultSetAnswer = preparedStatement.executeQuery();
                 while(resultSetAnswer.next()){
                     System.out.print(resultSetAnswer.getString("ANSWER_UID"));
