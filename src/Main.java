41322b70e2adcab61943db74cddc66729354848c
ParkChanhee1130 <kkazza1130@gmail.com>
2022-10-25 11:10:28 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         
-        String url = "jdbc:mysql://127.0.0.1:3306/project_01";
+        String url = "jdbc:mysql://127.0.0.1:3306/project_survey";
         String user = "root";
         String password = "*khacademy!";
         
