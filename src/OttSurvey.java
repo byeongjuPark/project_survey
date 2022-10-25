68385cd18fed0c5557255e33f861da452319ccd8
seonbuzz <ddonsdevelop@gmail.com>
2022-10-25 12:21:37 +0900

    incompletement ottsurvey

--- src/OttSurvey.java
+++ src/OttSurvey.java
@@ -6,56 +6,89 @@ public class OttSurvey{
 
       
       Scanner sc = new Scanner(System.in);
+
+      String input;
+      while(true){
+
+         System.out.println("-------------------------");
+         System.out.println("설문지 목록");
+         System.out.println("1. OTT 구독 서비스 만족도 \n 2.기타 \n 3. 기타");
+         System.out.println("-------------------------");
+
+         input = sc.next();
+         if(input.equals("1"){
+            OttSurvey(statement);
+            break;
+        }else{
+            System.out.println("* 잘못된 입력입니다.");
+            continue;
+      
+
       System.out.println("이름을 입력하세요");
       String name = sc.nextLine();
+
 
       System.out.println("– 모든 질문의 답은 한가지만 선택 가능합니다.");
       System.out.println("-------------------------------------------");
 
+      //질문 select 해서 담아온 객체
+      String query = "SELECT * FROM semi_project.questions";
+      ResultSet questionRs;
 
-      String query = "SELECT * FROM questions";
-      ResultSet resultset;
+      //답안 select 해서 담아온 객체
+      String query1 = "SELECT * FROM answers";
+      
+      ResultSet answerRs;
+
+      String query3 = "SELECT * FROM semi_project.participants WHERE PARTICIPANTS_UID IN(1,2,3)";
+
+      ResultSet parRs;
 
       try {
-         resultset = statement.executeQuery(query);
-        
-         //질문 문항 출력
-         while(resultset.next()){
-            String QUSTEION_UID = resultset.getString("QUESTION_UID");
-            System.out.println("QUESTION_UID:"+ QUSTEION_UID);
-            System.out.println("CONTENTS:"+resultset.getString("CONTENTS"));
+         questionRs = statement.executeQuery(query);
+         answerRs = statement.executeQuery(query1);
+         parRs = statement.executeQuery(query3);
 
-            String Q1 = "INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('1', '현재 구독중인 OTT 에서 제공하는 컨텐츠는 만족하는가')";
-            String Q2 = "INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('2', 'OTT에서 추천해주는 컨텐츠는 만족하는가')";
-            String Q3 = "INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('3', '업로드 주기는 적당하다고 생각하는가')";
-            String Q4 = "INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('4', '현재 OTT에서 제공하고 있는 자막 제공 서비스에 만족 하는가')";
-            String Q5 = "INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('5', '이용중인 구독료는 적당하다고 생각하는가')";
-            String Q6 = "INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('6', '향후 구독을 계속 유지할 생각이 있는가')";
-            String Q7 = "INSERT INTO `semi_project`.`questions` (`QUESTIONS_UID`, `CONTENTS`) VALUES ('7', '지인에게 해당 서비스를 추천할 의향이 있는가')";
-            statement.execute(Q1);
-            statement.execute(Q2);
-            statement.execute(Q3);
-            statement.execute(Q4);
-            statement.execute(Q5);
-            statement.execute(Q6);
-            statement.execute(Q7);
-               }
-      } catch (Exception e) {
-            e.printStackTrace();
-      }   
-      
-      
-      //화면단에 나올때는 문항과 답항이 한 번에 보여야 하는데 답안만 나오게 출력해야하는 것이 맞나? 모르겠음..
-      String query1 = "SELECT * FROM answers";
-      ResultSet resultset2;
+         //질문 문항 
+         while(questionRs.next()){  //next()는 다음 행이 있다면 true, while문이 true때까지 반복해서 돎.    
+          
+          //출력시 Q1. ~ 나옴.
+            String question_uid = questionRs.getString("QUESTION_UID");
+            System.out.println("Q"+ question_uid+".");
 
-         try {
-            resultset2 = statement.executeQuery(query1);
-         
+          //출력시 문항 내용 나옴.  
+            System.out.println("CONTENTS:"+questionRs.getString("CONTENTS"));
 
+            while(answerRs.next()){
+               String answer_uid = answerRs.getString("ANSWER_UID");
+               System.out.println("A"+answer_uid+".");
+               System.out.println("ANSWER"+answerRs.getString("ANSWERS"));
+
+             
+               String participants_uid = parRs.getString("PARTICIPANTS_UID");
+               System.out.println("P"+participants_uid+".");
+               System.out.println(name+parRs.getString("NAME"));
+
+               //답안을 스캐너로 받기
+
+               System.out.println("답:>>>");
+               String answer = sc.nextLine(); 
+
+       
+              //결과값
+              String query2 = "INSERT INTO result (PARTICIPANTS_UID, ANSWER_UID, QUESTIONS_UID) VALUES ('participants_uid', 'answer_uid', 'question_uid')";
+               statement.executeQuery(query2);
+            
+            
+              }
+              System.out.println();
+            }
          } catch (Exception e) {
-         }  
-
+      }  
+   }
+}
+   }
+}
             
          
       
@@ -69,5 +102,6 @@ public class OttSurvey{
       -- 사용자 답 입력
       INSERT INTO result (PARTICIPANTS_UID, ANSWER_UID, QUESTIONS_UID) VALUES ('1', '1', '1');*/
 
-   }
-}
+   
+
+
